765bd491d4e5c0266e573294d7450186dc6053b2
Lewis Fang <fang321654@hotmail.com>
2019-11-11 16:55:39 -0800

    initial commit

---
+++ app/src/main/java/com/example/safe_eats/RestaurantListViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.safe_eats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestaurantListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_list_view);
+    }
+}
